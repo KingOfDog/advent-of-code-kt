6533da4a5db8c3ea22a9b948423742a839a4142e
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-02 12:12:20 +0100

    Solve Day 2

--- app/src/main/kotlin/day02/Day02.kt
+++ app/src/main/kotlin/day02/Day02.kt
@@ -13,9 +13,55 @@ fun main(args: Array<String>) {
 
 
 fun solveDay02Part1(input: List<String>): Int {
-    TODO()
+    return input
+        .map { Command.fromString(it) }
+        .map { command ->
+            when (command.direction) {
+                "forward" -> Position(x = command.amount)
+                "down" -> Position(depth = command.amount)
+                "up" -> Position(depth = -command.amount)
+                else -> Position()
+            }
+        }
+        .sum()
+        .product()
 }
 
 fun solveDay02Part2(input: List<String>): Int {
-    TODO()
+    var aim = 0
+
+    return input
+        .map { Command.fromString(it) }
+        .mapNotNull { command ->
+            when (command.direction) {
+                "forward" -> Position(x = command.amount, depth = command.amount * aim)
+                "down" -> null.also { aim += command.amount }
+                "up" -> null.also { aim -= command.amount }
+                else -> null
+            }
+        }
+        .sum()
+        .product()
 }
+
+data class Command(val direction: String, val amount: Int) {
+    companion object {
+        fun fromString(input: String): Command {
+            val parts = input.split(" ")
+            val direction = parts[0]
+            val amount = parts[1].toInt()
+            return Command(direction, amount)
+        }
+    }
+}
+
+data class Position(val x: Int = 0, val depth: Int = 0) {
+    operator fun plus(pos: Position): Position {
+        return Position(x + pos.x, depth + pos.depth)
+    }
+
+    fun product(): Int = x * depth
+}
+
+private fun List<Position>.sum(): Position = reduce { acc, position -> acc + position }
+
