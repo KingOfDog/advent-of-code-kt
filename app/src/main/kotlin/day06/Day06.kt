cef6a975d24b921ca395875872df31ad5779ac9b
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-06 07:55:49 +0100

    Solve Day 6

--- app/src/main/kotlin/day06/Day06.kt
+++ app/src/main/kotlin/day06/Day06.kt
@@ -11,11 +11,67 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay06Part1, ::solveDay06Part2)
 }
 
-
 fun solveDay06Part1(input: List<String>): Int {
-    TODO()
+    val numberOfDays = 80
+    val fish = input.first().split(",").map { it.toInt() }
+        .overfishing(numberOfDays)
+    return howMuchIsTheFish(fish)
 }
 
-fun solveDay06Part2(input: List<String>): Int {
-    TODO()
+fun solveDay06Part2(input: List<String>): Long {
+    val numberOfDays = 256
+    val fishingNet = parseFishingNet(input)
+
+    for (d in 1..numberOfDays) {
+        decreaseAllCounters(fishingNet)
+        resetAndGiveBirth(fishingNet)
+    }
+
+    return howMuchIsTheFish(fishingNet)
 }
+
+private fun decreaseAllCounters(fishingNet: MutableMap<Int, Long>) {
+    for (i in 0..8) {
+        fishingNet[i - 1] = fishingNet.getOrZero(i)
+        fishingNet[i] = 0
+    }
+}
+
+private fun resetAndGiveBirth(fishingNet: MutableMap<Int, Long>) {
+    fishingNet[6] = fishingNet.getOrZero(6) + fishingNet.getOrZero(-1)
+    fishingNet[8] = fishingNet.getOrZero(8) + fishingNet.getOrZero(-1)
+    fishingNet[-1] = 0
+}
+
+@OptIn(ExperimentalStdlibApi::class)
+private fun parseFishingNet(input: List<String>): MutableMap<Int, Long> {
+    val fish = input.first().split(",").map { it.toInt() }
+    val fishingNet = buildMap<Int, Long> {
+        fish.forEach {
+            this.increase(it)
+        }
+    }.toMutableMap()
+    return fishingNet
+}
+
+private fun howMuchIsTheFish(fish: List<Int>) = fish.size
+private fun howMuchIsTheFish(fishingNet: Map<Int, Long>) = fishingNet.values.sum()
+
+private fun List<Int>.overfishing(
+    numberOfDays: Int
+): List<Int> {
+    var soundsFishy = this
+    for (d in 1..numberOfDays) {
+        val offspring = soundsFishy.filter { it == 0 }.map { 8 }
+        soundsFishy = soundsFishy.map { it - 1 }.map { if (it < 0) it + 7 else it }
+        soundsFishy = soundsFishy + offspring
+    }
+    return soundsFishy
+}
+
+fun MutableMap<Int, Long>.increase(key: Int) {
+    this[key] = getOrDefault(key, 0) + 1
+}
+
+private fun Map<Int, Long>.getOrZero(number: Int) =
+    getOrDefault(number, 0)
--- app/src/test/kotlin/day06/Day06Test.kt
+++ app/src/test/kotlin/day06/Day06Test.kt
@@ -6,11 +6,12 @@ import io.kotest.matchers.shouldBe
 class Day06Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "3,4,3,1,2"
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1: Int = 5934
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2: Long = 26984457539
 
     "Solving day 6" - {
         "part 1 for the sample input should return the correct output" {
