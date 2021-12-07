55335d52f61ebf99b580f8b2b235f7ab77e16042
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-07 09:03:09 +0100

    Solve Day 7

--- app/src/main/kotlin/day07/Day07.kt
+++ app/src/main/kotlin/day07/Day07.kt
@@ -3,6 +3,10 @@ package day07
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.roundToInt
 
 fun main(args: Array<String>) {
     val day = 7
@@ -13,9 +17,44 @@ fun main(args: Array<String>) {
 
 
 fun solveDay07Part1(input: List<String>): Int {
-    TODO()
+    val initialPositions = parseInput(input)
+    val targetPosition = initialPositions.median()
+    return initialPositions.sumOf { abs(targetPosition - it) }
 }
 
 fun solveDay07Part2(input: List<String>): Int {
-    TODO()
+    val initialPositions = parseInput(input)
+    val average = initialPositions.average().roundToInt()
+    val targetPositions = listOf(average - 1, average, average + 1)
+    return targetPositions.calculateCosts(initialPositions).minOf { it }
+}
+
+fun solveDay07Part2BruteForce(input: List<String>): Int {
+    val initialPositions = parseInput(input)
+    val possiblePositions = (initialPositions.minOrNull()!!..initialPositions.maxOrNull()!!).toList()
+    return possiblePositions.calculateCosts(initialPositions).minOf { it }
+}
+
+private fun List<Int>.calculateCosts(
+    initialPositions: List<Int>
+): List<Int> = map { targetPosition -> initialPositions.sumOf { fuelCost(abs(targetPosition - it)) } }
+
+private fun parseInput(input: List<String>): List<Int> {
+    val initialPositions = input.first().split(",").map { it.toInt() }
+    return initialPositions
+}
+
+fun List<Int>.median(): Int {
+    val sorted = sorted()
+    val middle = size / 2.0
+    return if (middle % 1 == 0.0) {
+        sorted[middle.toInt()]
+    } else {
+        (sorted[floor(middle).toInt()] + sorted[ceil(middle).toInt()]) / 2
+    }
+}
+
+fun fuelCost(distance: Int): Int {
+    if (distance == 0) return 0
+    return fuelCost(distance - 1) + distance
 }
