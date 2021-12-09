ad094073e7d6457c3f12c8f8187fd8d48b1a1ae4
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-09 08:07:31 +0100

    Solve Day 9

--- app/src/main/kotlin/day09/Day09.kt
+++ app/src/main/kotlin/day09/Day09.kt
@@ -11,11 +11,71 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay09Part1, ::solveDay09Part2)
 }
 
-
 fun solveDay09Part1(input: List<String>): Int {
-    TODO()
+    val heights = input.map { row -> row.toCharArray().map { it.digitToInt() } }
+    val width = heights[0].size
+    val height = heights.size
+    val lowPoints = mutableListOf<Int>()
+    heights.forEachIndexed { y, row ->
+        row.forEachIndexed { x, point ->
+            val surroundingPoints = listOf(0 to 1, 0 to -1, -1 to 0, 1 to 0)
+                .map { x + it.first to y + it.second }
+                .filterNot { it.first < 0 || it.second < 0 || it.first >= width || it.second >= height }
+                .map { heights[it.second][it.first] }
+            if (surroundingPoints.none { it <= point }) {
+                lowPoints.add(point)
+            }
+        }
+    }
+    return lowPoints.sumOf { it + 1 }
 }
 
 fun solveDay09Part2(input: List<String>): Int {
-    TODO()
+    val heights = input.map { row -> row.toCharArray().map { it.digitToInt() } }
+    val width = heights[0].size
+    val height = heights.size
+    val points = heights.mapIndexed { y, row -> row.mapIndexed { x, _ -> Point(x, y) } }.flatten()
+    val availablePoints = points.toMutableList()
+    val usedPoints = mutableListOf<Point>()
+
+    val basins = points.asSequence()
+        .filterNot { heights[it.y][it.x] == 9 }
+        .filter { availablePoints.contains(it) }
+        .map {
+            heights.findBasin(it, width, height, usedPoints).also { points ->
+                availablePoints.removeAll(points)
+                usedPoints.addAll(points)
+                println("Completed step for $it and found $points")
+            }
+        }
+        .toList()
+
+    return basins.map { it.size }.sortedDescending().subList(0, 3).reduce { acc, i -> acc * i }
+}
+
+data class Point(val x: Int, val y: Int)
+
+fun List<List<Int>>.findBasin(
+    point: Point,
+    width: Int,
+    height: Int,
+    alreadyVisited: List<Point> = emptyList()
+): List<Point> {
+    if (this[point.y][point.x] == 9) {
+        return emptyList()
+    }
+    val neighbors = point.getNeighbors(width, height).filterNot { alreadyVisited.contains(it) }
+    val visited = alreadyVisited + neighbors + listOf(point)
+    val neighborBasins = mutableSetOf<Point>()
+    for (neighbor in neighbors) {
+        val basin = this.findBasin(neighbor, width, height, visited + neighborBasins)
+        neighborBasins.addAll(basin)
+    }
+    return neighborBasins.toList() + listOf(point)
+}
+
+fun Point.getNeighbors(width: Int, height: Int): List<Point> {
+    return listOf(0 to 1, 0 to -1, -1 to 0, 1 to 0)
+        .map { Point(x + it.first, y + it.second) }
+        .filterNot { it.x < 0 || it.y < 0 || it.x >= width || it.y >= height }
 }
