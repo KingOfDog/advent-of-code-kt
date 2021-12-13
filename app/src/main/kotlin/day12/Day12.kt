3496fd0fa3813c68c85fe7a91ecf62ffa5a79d3f
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-13 07:14:09 +0100

    Solve Day 12

--- app/src/main/kotlin/day12/Day12.kt
+++ app/src/main/kotlin/day12/Day12.kt
@@ -3,6 +3,7 @@ package day12
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import java.nio.file.Path
 
 fun main(args: Array<String>) {
     val day = 12
@@ -13,9 +14,68 @@ fun main(args: Array<String>) {
 
 
 fun solveDay12Part1(input: List<String>): Int {
-    TODO()
+    return execute(input, canVisitSmallTwice = false)
+}
+
+private fun execute(input: List<String>, canVisitSmallTwice: Boolean): Int {
+    val caveConnections = input.map { line -> line.split("-") }.map { parts -> Cave(parts[0]) to Cave(parts[1]) }
+    val caveGraph = Graph<Cave>()
+    caveConnections.forEach { connection ->
+        caveGraph.addEdge(connection.first, connection.second)
+    }
+    val start = Cave("start")
+
+    val paths = caveGraph.getPossiblePaths(start, emptyList(), canVisitSmallTwice)
+
+    return paths.size
 }
 
 fun solveDay12Part2(input: List<String>): Int {
-    TODO()
+    return execute(input, canVisitSmallTwice = true)
+}
+
+private fun Graph<Cave>.getPossiblePaths(from: Cave, path: List<Cave>, canVisitSmallTwice: Boolean): List<List<Cave>> {
+    val pathWithSelf = path + from
+    if (from.isEnd) {
+        return listOf(pathWithSelf)
+    }
+
+    val possibilities = this.adjacencyMap.getOrDefault(from, emptySet())
+        .filterNot { it.isStart }
+        .filterNot { !canVisitSmallTwice && it.isSmall && path.contains(it) }
+    return possibilities.map {
+        getPossiblePaths(
+            it,
+            pathWithSelf,
+            canVisitSmallTwice = canVisitSmallTwice && (!it.isSmall || !path.contains(it))
+        )
+    }.flatten().toSet().toList()
+}
+
+class Graph<T> {
+    val adjacencyMap: HashMap<T, HashSet<T>> = HashMap()
+
+    fun addEdge(sourceVertex: T, destinationVertex: T) {
+        // Add edge to source vertex / node.
+        adjacencyMap
+            .computeIfAbsent(sourceVertex) { HashSet() }
+            .add(destinationVertex)
+        // Add edge to destination vertex / node.
+        adjacencyMap
+            .computeIfAbsent(destinationVertex) { HashSet() }
+            .add(sourceVertex)
+    }
+
+    override fun toString(): String = StringBuffer().apply {
+        for (key in adjacencyMap.keys) {
+            append("$key -> ")
+            append(adjacencyMap[key]?.joinToString(", ", "[", "]\n"))
+        }
+    }.toString()
+}
+
+data class Cave(val name: String) {
+    val isSmall = name.lowercase() == name
+    val isStart = name == "start"
+    val isEnd = name == "end"
 }
--- app/src/test/kotlin/day12/Day12Test.kt
+++ app/src/test/kotlin/day12/Day12Test.kt
@@ -1,24 +1,81 @@
 package day12
 
+import io.kotest.core.datatest.forAll
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 
 class Day12Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
+    val sampleInput1: List<String> = listOf(
+        "start-A",
+        "start-b",
+        "A-c",
+        "A-b",
+        "b-d",
+        "A-end",
+        "b-end",
+    )
+    val sampleInput2: List<String> = listOf(
+        "dc-end",
+        "HN-start",
+        "start-kj",
+        "dc-start",
+        "dc-HN",
+        "LN-dc",
+        "HN-end",
+        "kj-sa",
+        "kj-HN",
+        "kj-dc",
+    )
+    val sampleInput3 = listOf(
+        "fs-end",
+                "he-DX",
+                "fs-he",
+                "start-DX",
+                "pj-DX",
+                "end-zg",
+                "zg-sl",
+                "zg-pj",
+                "pj-he",
+                "RW-he",
+                "fs-DX",
+                "pj-RW",
+                "zg-RW",
+                "start-pj",
+                "he-WI",
+                "zg-he",
+                "pj-fs",
+                "start-RW",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1_1: Int = 10
+    val sampleSolutionPart1_2: Int = 19
+    val sampleSolutionPart1_3: Int = 226
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2_1: Int = 36
+    val sampleSolutionPart2_2: Int = 103
+    val sampleSolutionPart2_3: Int = 3509
 
     "Solving day 12" - {
+
         "part 1 for the sample input should return the correct output" {
-            solveDay12Part1(sampleInput) shouldBe sampleSolutionPart1
+            forAll(
+                sampleInput1 to sampleSolutionPart1_1,
+                sampleInput2 to sampleSolutionPart1_2,
+                sampleInput3 to sampleSolutionPart1_3
+            ) {
+                solveDay12Part1(it.first) shouldBe it.second
+            }
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay12Part2(sampleInput) shouldBe sampleSolutionPart2
+            forAll(
+                sampleInput1 to sampleSolutionPart2_1,
+                sampleInput2 to sampleSolutionPart2_2,
+                sampleInput3 to sampleSolutionPart2_3
+            ) {
+                solveDay12Part2(it.first) shouldBe it.second
+            }
         }
     }
 })
