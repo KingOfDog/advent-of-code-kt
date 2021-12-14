55787b3fbcc8c433c2f899e5fd701b4f95dad9ff
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-14 12:06:22 +0100

    Solve Day 14

--- app/src/main/kotlin/day14/Day14.kt
+++ app/src/main/kotlin/day14/Day14.kt
@@ -12,10 +12,89 @@ fun main(args: Array<String>) {
 }
 
 
-fun solveDay14Part1(input: List<String>): Int {
-    TODO()
+fun solveDay14Part1(input: List<String>): Long {
+    return execute(input, 10)
 }
 
-fun solveDay14Part2(input: List<String>): Int {
-    TODO()
+fun solveDay14Part2(input: List<String>): Long {
+    return execute(input, 40)
+}
+
+private fun execute(input: List<String>, steps: Int): Long {
+    val dictionary = input.subList(2, input.size).map { it.split(" -> ") }.associate { it[0] to it[1] }
+
+    var pairCounts = input.first().windowed(2).groupBy { it }.map { it.key to it.value.size.toLong() }.toMap()
+
+    repeat(steps) {
+        pairCounts = calculateNextStep(pairCounts, dictionary)
+    }
+
+    val elementCounts = getElementCounts(pairCounts, input)
+
+    val mostCommon = elementCounts.maxOfOrNull { it.value } ?: 0L
+    val leastCommon = elementCounts.minOfOrNull { it.value } ?: 0L
+
+    return mostCommon - leastCommon
+}
+
+private fun calculateNextStep(
+    pairCounts: Map<String, Long>,
+    dictionary: Map<String, String>
+): Map<String, Long> {
+    val nextPairCounts = pairCounts.toMutableMap()
+
+    pairCounts.forEach { (pair, count) ->
+        val insertElement = dictionary[pair]
+        val newPairLeft = pair.substring(0, 1) + insertElement
+        val newPairRight = insertElement + pair.substring(1, 2)
+
+        nextPairCounts.decreaseBy(pair, count)
+        nextPairCounts.increaseBy(newPairLeft, count)
+        nextPairCounts.increaseBy(newPairRight, count)
+    }
+
+    return nextPairCounts.toMap()
+}
+
+private fun getElementCounts(
+    pairCounts: Map<String, Long>,
+    input: List<String>
+): MutableMap<Char, Long> {
+    val elementCounts = mutableMapOf<Char, Long>()
+    pairCounts.forEach { (pair, count) ->
+        elementCounts[pair[0]] = elementCounts.getOrDefault(pair[0], 0L) + count
+    }
+    elementCounts[input.first().last()] = elementCounts[input.first().last()]!! + 1
+    return elementCounts
+}
+
+private fun executeSlow(input: List<String>, steps: Int): Long {
+    val dictionary = input.subList(2, input.size).map { it.split(" -> ") }.associate { it[0] to it[1] }
+
+    var template = input.first()
+    val nextTemplate = template.toCharArray().toMutableList()
+
+    repeat(steps) {
+        var insertIndex = 1
+        template.windowed(2) { pair ->
+            val insert = dictionary.getOrDefault(pair, "")
+            nextTemplate.addAll(insertIndex, insert.toCharArray().toList())
+            insertIndex += insert.length + 1
+        }
+
+        template = nextTemplate.toCharArray().concatToString()
+    }
+
+    val mostCommon = nextTemplate.groupBy { it }.maxOfOrNull { it.value.size.toLong() } ?: 0L
+    val leastCommon = nextTemplate.groupBy { it }.minOfOrNull { it.value.size.toLong() } ?: 0L
+
+    return mostCommon - leastCommon
+}
+
+fun MutableMap<String, Long>.increaseBy(key: String, add: Long) {
+    this[key] = getOrDefault(key, 0L) + add
+}
+
+fun MutableMap<String, Long>.decreaseBy(key: String, sub: Long) {
+    this[key] = getOrDefault(key, 0L) - sub
 }
--- app/src/test/kotlin/day14/Day14Test.kt
+++ app/src/test/kotlin/day14/Day14Test.kt
@@ -6,11 +6,29 @@ import io.kotest.matchers.shouldBe
 class Day14Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "NNCB",
+        "",
+        "CH -> B",
+        "HH -> N",
+        "CB -> H",
+        "NH -> C",
+        "HB -> C",
+        "HC -> B",
+        "HN -> C",
+        "NN -> C",
+        "BH -> H",
+        "NC -> B",
+        "NB -> B",
+        "BN -> B",
+        "BB -> N",
+        "BC -> B",
+        "CC -> N",
+        "CN -> C",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1: Long = 1588
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2: Long = 2188189693529
 
     "Solving day 14" - {
         "part 1 for the sample input should return the correct output" {
