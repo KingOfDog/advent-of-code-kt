576e5599ada224e5cf21ccf20757673ca6f8310a
Marcel Struck <marcel.struck@wetteronline.de>
2021-12-16 11:36:58 +0100

    Solve Day 16

--- app/src/main/kotlin/day16/Day16.kt
+++ app/src/main/kotlin/day16/Day16.kt
@@ -13,9 +13,162 @@ fun main(args: Array<String>) {
 
 
 fun solveDay16Part1(input: List<String>): Int {
-    TODO()
+    val packet = parsePacketFromInput(input)
+
+    return packet.versionSum()
 }
 
-fun solveDay16Part2(input: List<String>): Int {
-    TODO()
+fun solveDay16Part2(input: List<String>): Long {
+    val packet = parsePacketFromInput(input)
+
+    return packet.result()
+}
+
+private fun parsePacketFromInput(input: List<String>): Packet {
+    val hexadecimal = input.first()
+    val binary = hexadecimal.hexToBinary()
+
+    println(binary)
+
+    val packet = parsePacketType(binary)
+    packet.parseContent(binary.withoutHeader())
+    println(packet)
+    return packet
+}
+
+fun String.hexToBinary(): String {
+    val hexadecimalDigits = mapOf(
+        '0' to "0000",
+        '1' to "0001",
+        '2' to "0010",
+        '3' to "0011",
+        '4' to "0100",
+        '5' to "0101",
+        '6' to "0110",
+        '7' to "0111",
+        '8' to "1000",
+        '9' to "1001",
+        'A' to "1010",
+        'B' to "1011",
+        'C' to "1100",
+        'D' to "1101",
+        'E' to "1110",
+        'F' to "1111",
+    )
+
+    return map { hexadecimalDigits[it] }.joinToString("")
+}
+
+abstract class Packet(
+    val version: Int,
+    val typeID: Int,
+) {
+
+    abstract fun parseContent(content: String): String
+
+    open fun versionSum(): Int {
+        return version
+    }
+
+    abstract fun result(): Long
+}
+
+class ValuePacket(version: Int) : Packet(version, 4) {
+
+    var value: Long = 0
+
+    override fun parseContent(content: String): String {
+        var valueBits = ""
+
+        var remaining = content
+        var goOn = true
+        while (goOn && remaining.isNotEmpty()) {
+            if (remaining[0] == '0') goOn = false
+            valueBits += remaining.substring(1, 5)
+            remaining = remaining.removeRange(0, 5)
+        }
+
+        value = valueBits.toLong(2)
+
+        return remaining
+
+    }
+
+    override fun toString(): String {
+        return "ValuePacket($version, $value)"
+    }
+
+    override fun result(): Long = value
+}
+
+class OperatorPacket(version: Int, typeID: Int) : Packet(version, typeID) {
+
+    val subpackets = mutableListOf<Packet>()
+
+    override fun parseContent(content: String): String {
+        val lengthTypeID = content.substring(0, 1).toInt(2)
+
+        if (lengthTypeID == 0) {
+            val length = content.substring(1, 16).toInt(2)
+            var remaining = content.substring(16, 16 + length)
+            while (remaining.length > 6 && remaining.isNotBlank() && remaining.contains('1')) {
+                val packet = parsePacketType(remaining)
+                remaining = packet.parseContent(remaining.withoutHeader())
+                subpackets.add(packet)
+            }
+
+            return content.substring(16 + length, content.length)
+        } else if (lengthTypeID == 1) {
+            val count = content.substring(1, 12).toInt(2)
+            var remaining = content.substring(12, content.length)
+            repeat(count) {
+                val packet = parsePacketType(remaining)
+                remaining = packet.parseContent(remaining.withoutHeader())
+                subpackets.add(packet)
+            }
+
+            return remaining
+        }
+
+        return content
+    }
+
+    override fun toString(): String {
+        return "OperatorPacket($version, $typeID, $subpackets)"
+    }
+
+    override fun versionSum(): Int {
+        return version + subpackets.sumOf { it.versionSum() }
+    }
+
+    override fun result(): Long {
+        val results = subpackets.map { it.result() }
+        return when (typeID) {
+            0 -> results.sum()
+            1 -> results.reduce { acc, r -> acc * r }
+            2 -> results.minOf { it }
+            3 -> results.maxOf { it }
+            5 -> if (results[0] > results[1]) 1 else 0
+            6 -> if (results[0] < results[1]) 1 else 0
+            7 -> if (results[0] == results[1]) 1 else 0
+            else -> 0
+        }
+    }
+}
+
+fun parsePacketType(rawPacket: String): Packet {
+    val version = rawPacket.substring(0, 3).toInt(radix = 2)
+    val typeID = rawPacket.substring(3, 6).toInt(radix = 2)
+
+    val packet = if (typeID == 4) {
+        ValuePacket(version)
+    } else {
+        OperatorPacket(version, typeID)
+    }
+
+    return packet
+}
+
+fun String.withoutHeader(): String {
+    return substring(6, length)
 }
--- app/src/test/kotlin/day16/Day16Test.kt
+++ app/src/test/kotlin/day16/Day16Test.kt
@@ -5,20 +5,34 @@ import io.kotest.matchers.shouldBe
 
 class Day16Test : FreeSpec({
 
-    val sampleInput: List<String> = listOf(
+    val sampleInputsPart1 = mapOf(
+        "8A004A801A8002F478" to 16,
+        "620080001611562C8802118E34" to 12,
+        "C0015000016115A2E0802F182340" to 21,
+        "A0016C880162017C3686B18A3D4780" to 31,
     )
-
-    val sampleSolutionPart1: Int = -1
-
-    val sampleSolutionPart2: Int = -1
+    val sampleInputsPart2 = mapOf(
+        "C200B40A82" to 3,
+        "04005AC33890" to 54,
+        "880086C3E88112" to 7,
+        "CE00C43D881120" to 9,
+        "D8005AC2A8F0" to 1,
+        "F600BC2D8F" to 0,
+        "9C005AC2F8F0" to 0,
+        "9C0141080250320F1802104A08" to 1,
+    )
 
     "Solving day 16" - {
         "part 1 for the sample input should return the correct output" {
-            solveDay16Part1(sampleInput) shouldBe sampleSolutionPart1
+            sampleInputsPart1.forEach { input, expected ->
+                solveDay16Part1(listOf(input)) shouldBe expected
+            }
         }
 
         "part 2 for the sample input should return the correct output" {
-            solveDay16Part2(sampleInput) shouldBe sampleSolutionPart2
+            sampleInputsPart2.forEach { input, expected ->
+                solveDay16Part2(listOf(input)) shouldBe expected
+            }
         }
     }
 })
